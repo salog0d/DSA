88ffccf36a3ae2a7b8b1c30471efcd1c2e3eacb6
salogod <david.salomon.nava11@gmail.com>
2024-12-18 17:47:02 -0600

    New method explaining n to the second order

--- Time_Complexity/o_notation.cpp
+++ Time_Complexity/o_notation.cpp
@@ -12,6 +12,26 @@ int sum(int A[] , int n){
 
 /* Time complexity of O(n2)*/
 
+void swap(x,y){
+    int t;
+    t=x;// 1
+    x=y// 1
+    y=t// 1
+}
+
+/* Time complexity of O(1)*/
+
+void add(int n){
+    int i,j;
+    for(i=0 ; i<n ;i++){// n+1
+        for(j=0 ; j<n ; j++){ // n x (n+1) 
+            c[i][j] = A[i][j] + B[i][j]; // n x n
+        }
+    }
+}
+
+/* Time complexity of O(n2)*/
+
 int main(){
     int B [] = {1,2,3,4,5,6};
     int c = 5;
