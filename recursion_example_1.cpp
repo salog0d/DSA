2fddafd6b43c215db36e373212ebbd64f79d571e
Salog0d <david.salomon.nava11@gmail.com>
2024-12-18 01:30:12 -0600

    Basics of recursion

--- recursion_example_1.cpp
+++ recursion_example_1.cpp
@@ -7,8 +7,22 @@ void recursive(int n){
         recursive(n-1);
     }
 }
+
+void recursive2(int n){
+    if(n>0){
+        recursive2(n-1);
+        cout<<n<<endl;
+    }
+}
+
+/*Loop only ascending phase, and recursion has ascending and descending
+return tuime, making it more moldeable to resolve probelms such as the Finobasi
+seccunce wich is a piramid wich both ascends and descends.
+*/
+
+
 int main()
 {
-        int x = 7;
-        recursive(x);
+        int x = 3;
+        recursive2(x);
 }
